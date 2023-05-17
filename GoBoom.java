9c805656c8e66f4af604958aa69863c74bebdc3c
Siddiq Ferhad <123736322+siddiq-ferhad@users.noreply.github.com>
2023-05-17 20:15:26 +0800

    Update GoBoom.java

--- GoBoom.java
+++ GoBoom.java
@@ -1,1 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GoBoom {
+    public static void main(String[] args) {
+        List<String> deck = new ArrayList<>();
+        List<List<String>> players = new ArrayList<>();
+        int numPlayers = 4;
+
+        initializeDeck(deck);
+        shuffleDeck(deck);
+
+        for (int i = 0; i < numPlayers; i++) {
+            List<String> playerCards = new ArrayList<>();
+            players.add(playerCards);
+
+            for (int j = 0; j < 7; j++) {
+                playerCards.add(deck.remove(0));
+            }
+        }
+
+        // Print the cards for each player
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.println("Player " + (i + 1) + ": " + players.get(i));
+        }
+    }
+
+    private static void initializeDeck(List<String> deck) {
+        String[] suits = {"h", "d", "c", "s"};
+        String[] ranks = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck.add(suit + rank);
+            }
+        }
+    }
+
+    private static void shuffleDeck(List<String> deck) {
+        Collections.shuffle(deck);
+    }
+}
 
