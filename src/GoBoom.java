9b27ebd0afe47c498b80eceef1fa1ca4f7be1c51
Siddiq Ferhad <123736322+siddiq-ferhad@users.noreply.github.com>
2023-05-21 19:53:32 +0800

    Update GoBoom.java

--- src/GoBoom.java
+++ src/GoBoom.java
@@ -1,3 +1,16 @@
+/*
+No | Feature                                                                         | Completed (Y/N)
+-- | ------------------------------------------------------------------------------- | ---------------
+1  | All cards should be faced up to facilitate checking.                            |        Y
+2  | Start a new game with randomized 52 cards.                                      |        Y
+3  | The first card in the deck is the first lead card and is placed at the center.  |        Y
+4  | The first lead card determines the first player.                                |        Y
+5  | Deal 7 cards to each of the 4 players.                                          |        Y
+6  | All players must follow the suit or rank of the lead card.                      |        Y
+7  | The highest-rank card with the same suit as the lead card wins the trick.       |        Y
+8  | The winner of a trick leads the next card.                                      |        Y
+*/
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
